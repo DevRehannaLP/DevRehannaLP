8bb518b632d00ce8d3ae697661c27033762c81cf
DevRehannaLP <rlportee83@gmail.com>
2026-03-08 04:26:39 -0500

    Add bash system check script - date, user, uptime, and disk usuage

---
+++ 1_projects/systemcheck_bash/systemcheck.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "=== System Check ==="
+echo "Date: $(date)"
+echo "User: $(whoami)"
+echo "Directory: $(pwd)"
+echo "Uptime: $(uptime -p)"
+echo "Disk Usage: $(df -h ~ | tail -1 | awk '{print $5}') used"

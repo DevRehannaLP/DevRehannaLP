b0ab5a5ffeaa37fac09eb2f7f1ec9bce1e08f6e9
DevRehannaLP <rlportee83@gmail.com>
2026-03-12 09:02:40 -0500

    Portfolio restructure - real work only, clean foundation

---
+++ Projects/bash/signature.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ∞ ☬ DevRehannaLP ☬ | DevSecOps ∞ #
+
+#Color codes
+CYAN='\033[0;36m'
+PURPLE='\033[0;35m'
+RESET='\033[0m'
+
+echo -e "\n${CYAN}# \u221e ${PURPLE}\u262cDevRehannaLP\u262c${CYAN} | DevSecOps \u221e #${RESET}\n"
